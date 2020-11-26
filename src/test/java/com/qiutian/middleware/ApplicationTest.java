7ca4e8e524a4ff4029826dd4850fee83c596dc68
qiutian <qiutian@baofu.com>
2020-11-26 14:48:32 +0800

    配置Redis

--- src/test/java/com/qiutian/middleware/ApplicationTest.java
+++ src/test/java/com/qiutian/middleware/ApplicationTest.java
@@ -40,18 +40,6 @@ public class ApplicationTest extends TestCase {
         System.out.println(System.getProperty("java.library.path"));
 
 
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(1);
-        fixedThreadPool.execute(() -> {
-            System.out.println("==========00000");
-            System.load("D:\\Ths\\THSDataInterface_Windows\\bin\\x64\\iFinDJava_x64.dll");
-            int ret = JDIBridge.THS_iFinDLogin("mdzg002", "686375");
-            log.info("登录返回结果 + " + Thread.currentThread().getName() + "：{}");
-            String result = JDIBridge.THS_RealtimeQuotes("111330.OF,700002.OF", "real_time_valuation");
-            ThsValuationResDTO resDTO = JSON.parseObject(result, ThsValuationResDTO.class);
-            System.out.println("=========" + Thread.currentThread().getName() + "============" + JSON.toJSONString(resDTO));
-            JDIBridge.THS_iFinDLogout();
-            System.out.println("THS_iFinDLogout ==> ");
-        });
 
 
 
